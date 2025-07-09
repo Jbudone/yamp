6b46a5c37cfea63d1de69bc7d9bc20dd32450af9
Josh Braendel <Jbud@live.ca>
2025-07-08 20:49:28 -0700

    cache signed url; sign the entire path; check for changes to resign

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -7,37 +7,29 @@ import config from '$lib/config';
 export default class Player {
 
     private instance: player;
+    private pathToken: object;
 
     private constructor() {
         console.log('Player instance created');
+
+        this.pathToken = {};
     }
 
     async init() {
         this.player = MediaPlayer().create();
 
-
         // FIXME: hardcoded song for testing
         let songId = 'song_5294';
-        var securityKey = config.BUNNYCDN_GET_ACCESS_KEY!;
-        var songPath = `${config.BUNNYCDN_GET_HOST!}/${songId}`;
-        var mpdUrl = `${songPath}/manifest.mpd`;
 
-        // FIXME: hardcoded values, mostly copy/paste from bunnycdn
-        const SIGNING_EXPIRATION = 7200;
-        const SIGNING_USER_IP = '';
-        const SIGNING_IS_DIRECTORY = false;
-        const SIGNING_PATH_ALLOWED = '/';
-        const SIGNING_FOR_COUNTRY = 'CA,US';
-        const SIGNING_EXCLUDE_COUNTRY = 'JP';
-
-        var signed = await this.signUrl(mpdUrl, securityKey, SIGNING_EXPIRATION, SIGNING_USER_IP, SIGNING_IS_DIRECTORY, SIGNING_PATH_ALLOWED, SIGNING_FOR_COUNTRY, SIGNING_EXCLUDE_COUNTRY);
-        this.player.initialize(document.querySelector('video'), signed, true);
+        const url = await this.buildURLPath(songId, 'manifest.mpd');
+        this.player.initialize(document.querySelector('video'), url, true);
 
         const interceptor: RequestInterceptor = async (request: CommonMediaRequest) => {
-            var u = new URL(request.url);
-            var url = `${songPath}/` + u.pathname.substr(u.pathname.lastIndexOf('/')+1);
-            var signed = await this.signUrl(url, securityKey, SIGNING_EXPIRATION, SIGNING_USER_IP, SIGNING_IS_DIRECTORY, SIGNING_PATH_ALLOWED, SIGNING_FOR_COUNTRY, SIGNING_EXCLUDE_COUNTRY);
-            request.url = signed;
+
+            const u = new URL(request.url);
+            const resource = u.pathname.substr(u.pathname.lastIndexOf('/')+1);
+
+            request.url = await this.buildURLPath(songId, resource);
             console.log(request.url);
 
             return Promise.resolve(request)
@@ -46,81 +38,56 @@ export default class Player {
         this.player.play();
     }
 
-    private addCountries(url, a, b) {
-        var tempUrl = url;
-        if (a != null) {
-            var tempUrlOne = new URL(tempUrl);
-            tempUrl += ((tempUrlOne.search == "") ? "?" : "&") + "token_countries=" + a;
-        }
-        if (b != null) {
-            var tempUrlTwo = new URL(tempUrl);
-            tempUrl += ((tempUrlTwo.search == "") ? "?" : "&") + "token_countries_blocked=" + b;
-        }
-        return tempUrl;
+    private getURLBasePath(songId) {
+        return `/${songId}/`;
     }
 
-    // FIXME: hardcoded values, mostly copy/paste from bunnycdn
-    private async signUrl(url, securityKey, expirationTime = 3600, userIp, isDirectory = false, pathAllowed, countriesAllowed, countriesBlocked) {
-        /*
-            url: CDN URL w/o the trailing '/' - exp. http://test.b-cdn.net/file.png
-            securityKey: Security token found in your pull zone
-            expirationTime: Authentication validity (default. 86400 sec/24 hrs)
-            userIp: Optional parameter if you have the User IP feature enabled
-            isDirectory: Optional parameter - "true" returns a URL separated by forward slashes (exp. (domain)/bcdn_token=...)
-            pathAllowed: Directory to authenticate (exp. /path/to/images)
-            countriesAllowed: List of countries allowed (exp. CA, US, TH)
-            countriesBlocked: List of countries blocked (exp. CA, US, TH)
-        */
-        var parameterData = "", parameterDataUrl = "", signaturePath = "", hashableBase = "", token = "";
-        var expires = Math.floor(new Date() / 1000) + expirationTime;
-        var url = this.addCountries(url, countriesAllowed, countriesBlocked);
-        var parsedUrl = new URL(url);
-        var parameters = (new URL(url)).searchParams;
-        if (pathAllowed != "") {
-            signaturePath = pathAllowed;
-            parameters.set("token_path", signaturePath);
+    private async buildURLPath(songId, resource) {
+        const signedURLPath = await this.getSignedURLPath(songId);
+        const basePath = this.getURLBasePath(songId);
+        return signedURLPath + basePath + resource;
+    }
+
+    private async getSignedURLPath(songId) {
+
+        const securityKey = config.BUNNYCDN_GET_ACCESS_KEY!;
+        const host = config.BUNNYCDN_GET_HOST!;
+
+        // FIXME: hardcoded expiration; think of something reasonable later
+        const SIGNING_EXPIRATION = 7200;
+
+        let signedURLPath = "";
+        if (this.pathToken && this.pathToken.songId == songId) {
+            signedURLPath = this.pathToken.signedURLPath;
         } else {
-            signaturePath = decodeURIComponent(parsedUrl.pathname);
+            // update signed token for song path
+            const basePath = this.getURLBasePath(songId);
+            const signedToken = await this.signURLPath(securityKey, SIGNING_EXPIRATION, basePath);
+            signedURLPath = host + "/" + signedToken;
+            this.pathToken.signedURLPath = signedURLPath;
+            this.pathToken.songId = songId;
+            this.pathToken.expires = 0; // FIXME; don't forget to take this into consideration in segment fetches (eg. 3 hour long song and token expires in 1 hour)
         }
-        parameters.sort();
 
-        var parameterData = '';
-        var a = new URLSearchParams(); 
-        if (Array.from(parameters).length > 0) {
-            parameters.forEach(function(value, key) {
-                if (value == "") {
-                    return;
-                }
-                if (parameterData.length > 0) {
-                    parameterData += "&";
-                }
-                a.append(key, value);
+        return signedURLPath;
+    }
 
-                parameterData += key + "=" + value;
-        //        parameterDataUrl += "&" + key + "=" + value;// queryString.escape(value);
-        //        
-            });
-        }
-        var parameterDataUrl = a.toString();
+    private async signURLPath(securityKey, expirationTime = 3600, signaturePath) {
 
-        
-        hashableBase = securityKey + signaturePath + expires + ((userIp != null) ? userIp : "") + parameterData;
+        const expires = Math.floor(new Date() / 1000) + expirationTime;
+        const hashableBase = securityKey + signaturePath + expires + 'token_path=' + signaturePath;
 
-        var data = (new TextEncoder()).encode(hashableBase) 
-        var hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
-        var hashArray = new Uint8Array(hashBuffer);
-        var base64String = btoa(String.fromCharCode(...hashArray));
+        const data = (new TextEncoder()).encode(hashableBase) 
+        const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
+        const hashArray = new Uint8Array(hashBuffer);
+        const base64String = btoa(String.fromCharCode(...hashArray));
 
-        var token = base64String;
-        token = token.replace(/\n/g, "").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+        // BunnyCDN requires fixing up base64 string
+        const token = base64String.replace(/\n/g, "").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 
-        //token = Buffer.from(crypto.createHash("sha256").update(hashableBase).digest()).toString("base64");
-        //token = token.replace(/\n/g, "").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
-        if (isDirectory) {
-            return parsedUrl.protocol+ "//" + parsedUrl.host + "/bcdn_token=" + token + '&' + parameterDataUrl + "&expires=" + expires + parsedUrl.pathname;
-        } else {
-            return parsedUrl.protocol + "//" + parsedUrl.host + parsedUrl.pathname + "?token=" + token + '&' + parameterDataUrl + "&expires=" + expires;
-        }
+        // isDirectory: Optional parameter - "true" returns a URL separated by forward slashes (exp. (domain)/bcdn_token=...)
+        // NOTE: bcdn_token indicates the beginning portion of the URL is signed for the path, and the trailing portion is for the filepath
+        return "bcdn_token=" + token + "&expires=" + expires + '&token_path=' + signaturePath.replace(/\//g, '%2F');
     }
 };
 
