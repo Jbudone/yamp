2c44d84d40a07f13cc24bbff5df32a3b93f6ba69
Josh Braendel <Jbud@live.ca>
2025-08-09 16:59:59 -0700

    limit on indexeddb cache size

--- src/lib/dbController.svelte.ts
+++ src/lib/dbController.svelte.ts
@@ -1,6 +1,11 @@
 import config from '$lib/config';
 import EventEmitter from 'eventemitter3';
 
+
+const INDEXEDDB_CACHESWEEP_INTERVAL = 30000;
+const TABLENAME_RESOURCES = 'cachedResources';
+const TABLENAME_META = 'cacheMeta';
+
 class DBController {
     private static instance: DBController;
     public EE: EventEmitter;
@@ -11,6 +16,8 @@ class DBController {
         return this.instance || (this.instance = new DBController());
     }
 
+    private pendingClean;
+
     async initialize() {
         console.log(config);
         this.EE = new EventEmitter();
@@ -20,6 +27,7 @@ class DBController {
 
             dbInitRequest.onsuccess = (e) => {
                 this.db = dbInitRequest.result;
+                window['db'] = this.db;
                 resolve();
             };
 
@@ -31,15 +39,30 @@ class DBController {
             dbInitRequest.onupgradeneeded = (e) => {
                 // initial creation or version change
                 let db = e.target.result;
+                let pendingOps = 0;
 
-                const cacheStore = db.createObjectStore('cachedResources', { keyPath: "songIdAndResource" });
+                ++pendingOps;
+                if (db.objectStoreNames.contains(TABLENAME_RESOURCES)) db.deleteObjectStore(TABLENAME_RESOURCES);
+                const cacheStore = db.createObjectStore(TABLENAME_RESOURCES, { keyPath: "songIdAndResource" });
                 cacheStore.createIndex('songId', 'songId', { unique: false });
                 cacheStore.createIndex('resource', 'resource', { unique: false });
 
-                cacheStore.transaction.oncomplete = (e) => {
-                    this.db = db;
-                    resolve();
+                ++pendingOps;
+                if (db.objectStoreNames.contains(TABLENAME_META)) db.deleteObjectStore(TABLENAME_META);
+                const cacheMetaStore = db.createObjectStore(TABLENAME_META, { keyPath: "songId" });
+                cacheMetaStore.createIndex('songId', 'songId', { unique: false });
+                cacheMetaStore.createIndex('datePlayed', 'datePlayed', { unique: false });
+                cacheMetaStore.createIndex('size', 'size', { unique: false });
+
+                const onPendingFinished = () => {
+                    if (--pendingOps == 0) {
+                        this.db = db;
+                        resolve();
+                    }
                 };
+
+                cacheStore.transaction.oncomplete = onPendingFinished;
+                cacheMetaStore.transaction.oncomplete = onPendingFinished;
             };
         }));
     }
@@ -57,7 +80,7 @@ class DBController {
 
     public async getCache(songId, resource) {
         return new Promise((resolve, reject) => {
-            const request = this.db.transaction(['cachedResources'], 'readonly').objectStore('cachedResources').get(`${songId}-${resource}`);
+            const request = this.db.transaction([TABLENAME_RESOURCES], 'readonly').objectStore(TABLENAME_RESOURCES).get(`${songId}-${resource}`);
             request.onerror = (e) => {
                 resolve(false);
             };
@@ -68,22 +91,99 @@ class DBController {
         });
     }
 
+    public async getMeta(songId) {
+        return new Promise((resolve, reject) => {
+            const request = this.db.transaction([TABLENAME_META], 'readonly').objectStore(TABLENAME_META).get(`${songId}`);
+            request.onerror = (e) => {
+                resolve(null);
+            };
+
+            request.onsuccess = (e) => {
+                resolve(request.result);
+            };
+        });
+    }
+
     public async addCache(songId, resource, data) {
         return new Promise((resolve, reject) => {
-            const request = this.db.transaction(['cachedResources'], 'readwrite').objectStore('cachedResources').add({
+            const request = this.db.transaction([TABLENAME_RESOURCES], 'readwrite').objectStore(TABLENAME_RESOURCES).add({
                 songIdAndResource: `${songId}-${resource}`,
                 songId: songId,
                 resource: resource,
                 data: data
             });
+
             request.onerror = (e) => {
                 resolve(false);
             };
 
-            request.onsuccess = (e) => {
-                resolve(true);
+            request.onsuccess = async (e) => {
+                const resourceSize = data.length;
+                const meta = await this.getMeta(songId);
+
+                let totalSize = (meta ? meta.size : 0) + resourceSize;
+                let requestMeta = this.db.transaction([TABLENAME_META], 'readwrite').objectStore(TABLENAME_META).put({
+                    songId: songId,
+                    datePlayed: Date.now(),
+                    size: totalSize
+                });
+
+                requestMeta.onerror = (e) => {
+                    resolve(false);
+                };
+
+                requestMeta.onsuccess = (e) => {
+                    this.queueCleanCache();
+                    resolve(true);
+                };
             };
         });
+    }
+
+    public async queueCleanCache() {
+        if (this.pendingClean) return;
+
+        this.pendingClean = setTimeout(async () => {
+            this.cleanCache();
+            this.pendingClean = null;
+        }, INDEXEDDB_CACHESWEEP_INTERVAL);
+    };
+
+    public async cleanCache() {
+        console.log("Sweeping cache");
+
+        const requestMeta = this.db.transaction([TABLENAME_META], 'readwrite').objectStore(TABLENAME_META).getAll();
+        requestMeta.onsuccess = async (e) => {
+            const results = e.target.result;
+            results.sort((a, b) => { return a.datePlayed < b.datePlayed; }); // sorted datePlayed descending
+            let totalUsage = 0;
+            for (let i = 0; i < results.length; ++i) {
+                totalUsage += results[i].size;
+                if (totalUsage > config.INDEXEDDB_MAXSIZE) {
+
+                    const _db = this.db; // FIXME: yuck
+                    await new Promise((resolve) => {
+                        const cursorRequest = _db.transaction([TABLENAME_RESOURCES], 'readwrite').objectStore(TABLENAME_RESOURCES).index('songId').openCursor(IDBKeyRange.only(results[i].songId));
+                        cursorRequest.onsuccess = function(event) {
+                            const cursor = event.target.result;
+                            if (cursor) {
+                                // Delete the current record
+                                cursor.delete();
+                                console.log(`Deleting song cached resource: ${cursor.primaryKey}`);
+
+                                // Move to the next record
+                                cursor.continue();
+                            } else {
+                                _db.transaction([TABLENAME_META], 'readwrite').objectStore(TABLENAME_META).delete(results[i].songId);
+                                resolve();
+                            }
+                        };
+                    });
+
+                    console.log(`Removed song cache: ${results[i].songId} - ${results[i].size} bytes`);
+                }
+            }
+        };
     }
 
     public async updateSongPlayed(songId) {
