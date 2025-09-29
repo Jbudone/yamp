65050f8e38ab19e0ec54a067d79097b897dafb18
Josh Braendel <Jbud@live.ca>
2025-09-29 09:59:44 -0700

    clear filter

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -29,11 +29,21 @@ const filterText = (e) => {
     LibraryController.filterText(text);
 };
 
+const clearFilterText = (e) => {
+    document.getElementById('libraryTextFilter').value = '';
+    LibraryController.filterText("");
+};
+
 </script>
 
 <div id='sidebar' class='mx-4 mt-10'>
 <div id='playlists' class=''>
-<input type='text' id='libraryTextFilter' on:input={filterText} class='bg-transparent rounded-3xl h-8 w-full p-4 border-2 text-slate-0 border-slate-800 ' />
+    <div id='libraryTextFilterContainer' class='flex'>
+        <input type='text' id='libraryTextFilter' on:input={filterText} class='bg-transparent rounded-3xl h-8 w-full p-4 border-2 text-slate-0 border-slate-800 ' />
+        <div id='controlsClearFilter' on:click={clearFilterText} class='-translate-x-9 translate-y-2'>
+            <svg class="icon w-6 h-6 cursor-pointer"> <use href="icons.svg#icon-repeat"></use> </svg>
+        </div>
+    </div>
 </div>
 </div>
 
